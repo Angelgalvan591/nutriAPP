6093f00a9df17c5110b198149001c642bd6b3767
Wendy-Vazquez <23308060610440@cetis61.edu.mx>
2025-11-04 11:05:55 -0700

    DISEÑO OF

--- app.py
+++ app.py
@@ -6,6 +6,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/recetas')
+def recetas():
+    return render_template('recetas.html')
+
+@app.route('/contadorcal')
+def contadorcal():
+    return render_template('contadorcal.html')
+
+@app.route('/iniciosesion')
+def iniciosesion():
+    return render_template('iniciosesion.html')
+
+@app.route('/crearcuenta')
+def crearcuenta():
+    return render_template('crearcuenta.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
